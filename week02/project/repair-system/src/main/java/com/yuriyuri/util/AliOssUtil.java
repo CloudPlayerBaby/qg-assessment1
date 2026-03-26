ae814e290584036bd1a7587611bb8ebd08587fab
CloudPlayerBaby <2656756196@qq.com>
2026-03-26 16:12:31 +0800

    第三次提交，完成了week2内容的基础篇 给repair_form表增加了image_url字段 图片上传使用了阿里云，密钥不能git上传所以可能要自己配一下才能用 前端代码全是ai写的，现在如果前端提交的数据出错会直接在后端报错，后续我会学习日志然后打印

--- week02/project/repair-system/src/main/java/com/yuriyuri/util/AliOssUtil.java
+++ week02/project/repair-system/src/main/java/com/yuriyuri/util/AliOssUtil.java
@@ -6,18 +6,24 @@ import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
 import com.aliyun.oss.model.PutObjectRequest;
 import com.aliyun.oss.model.PutObjectResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+@Component
 public class AliOssUtil {
+
 
     // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
     private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
     //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private static final String ACCESS_KEY_ID="LTAI5t7VS63eiWaDgg2zpR5B";
-    private static final String ACCESS_KEY_SECRET="QXdCo12wlG6basaZn08MotDHKqPBOs";
+    @Value("${aliyun.oss.access-key-id}")
+    private static final String ACCESS_KEY_ID = "LTAI5t7VS63eiWaDgg2zpR5B";
+    @Value("${aliyun.oss.access-key-secret}")
+    private static final String ACCESS_KEY_SECRET = "QXdCo12wlG6basaZn08MotDHKqPBOs";
     // 填写Bucket名称，例如examplebucket。
     private static final String BUCKET_NAME = "repair-system-demo";
 
@@ -25,7 +31,7 @@ public class AliOssUtil {
 
 
         // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(ENDPOINT,ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+        OSS ossClient = new OSSClientBuilder().build(ENDPOINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
         String url = "";
         try {
             // 填写字符串。
@@ -43,7 +49,7 @@ public class AliOssUtil {
             // 上传字符串。
             PutObjectResult result = ossClient.putObject(putObjectRequest);
             //url组成: https://bucket名称.区域节点/objectName
-            url = "https://"+BUCKET_NAME+"."+ENDPOINT.substring(ENDPOINT.lastIndexOf("/")+1)+"/"+objectName;
+            url = "https://" + BUCKET_NAME + "." + ENDPOINT.substring(ENDPOINT.lastIndexOf("/") + 1) + "/" + objectName;
         } catch (OSSException oe) {
             System.out.println("Caught an OSSException, which means your request made it to OSS, "
                     + "but was rejected with an error response for some reason.");
